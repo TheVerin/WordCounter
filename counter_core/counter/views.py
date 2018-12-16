689eda2690c1557c0567f177207db867719004e0
TheVerin <tomekbeben95@gmail.com>
2018-12-16 21:24:28 +0100

    Changes in plot making way - now it is working on data frame and it is also sorted by highest value

--- counter_core/counter/views.py
+++ counter_core/counter/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from counter.models import WoCount
 import matplotlib.pyplot as plt
+import seaborn as sns
 import re
+import pandas as pd
 
 
 def word_changer(text='') -> list:
@@ -15,33 +17,41 @@ def word_changer(text='') -> list:
     return words
 
 
-def word_dict(words: list):
+def word_df(words: list):
     found = {}
     words_set = set(words)
     for i in words:
         if i in words_set:
             found.setdefault(i, 0)
             found[i] += 1
-    return found
+
+    words_list = []
+    values_list = []
+    for key in found:
+        words_list.append(key)
+        values_list.append(found[key])
+
+    found_df = pd.DataFrame({'word': words_list, 'value': values_list})
+    found_df = found_df.sort_index(by=['value'], ascending=False)
+    return found_df
 
 
-def plot_maker(found: dict):
-    plt.bar(range(len(found)), found.values(), align='center')
-    plt.xticks(range(len(found)), list(found.keys()))
+def plot_maker(found_df):
+    plt.bar(data=found_df, x='word', height='value')
     return plt.show()
 
 
 def final(request):
     title = ''
     text = ''
-    result = ''
+    result_pre = 'Wait a second ;)'
 
     if request.GET.get('title'):
         title = request.GET.get('title')
         if request.GET.get('text'):
             text = request.GET.get('text')
             a = word_changer(text)
-            b = word_dict(a)
+            b = word_df(a)
             result = plot_maker(b)
 
     obj = WoCount.objects.create(title=title, text=text)
@@ -53,6 +63,6 @@ def final(request):
         {
             'title': title,
             'text': text,
-            'result': result
+            'result': result_pre
         }
     )
