4a884cfc231c1a226aaa5d3ed8f5f95584304fab
TheVerin <tomekbeben95@gmail.com>
2018-12-16 14:19:10 +0100

    Database is done - changes in models.py

--- counter_core/counter/models.py
+++ counter_core/counter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class WoCount(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    class Meta:
+        db_table = 'counter'
+
+    def __str__(self):
+        return self.title
